c56cfe349a84ed6886d32add51516358e15ac0c2
deepfriedbrain1 <afernan5@mail.sfsu.edu>
2018-06-06 21:36:29 -0700

    added Bullet class to project

--- src/src/Bullet.java
+++ src/src/Bullet.java
@@ -1,10 +1,25 @@
-
 package src;
 
+import java.awt.Rectangle;
+
 /**
- *
+ * Bullet class - polygonal shape of a bullet
  * @author Alberto Fernandez Saucedo
  */
-public class Bullet {
+public class Bullet extends BaseVectorShape {
     
-}
+    // no args constructor
+    Bullet(){
+        setShape(new Rectangle(0, 0, 1, 1));
+        setAlive(false);
+    }//end no args constructor
+    
+    // bounding rectangle
+    public Rectangle getBounds(){
+        Rectangle rect;
+        rect = new Rectangle((int)getX(), (int)getY(), 1, 1);
+        return rect;
+        
+    }// end getBounds
+    
+}//end Bullet
